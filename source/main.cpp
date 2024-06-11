42456fd98a194a0c88be5137727c82cde937e8d8
WuChang <3142324836@qq.com>
2024-06-11 16:15:18 +0800

    Init project

---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <event2/bufferevent.h>
+#include <event2/bufferevent_ssl.h>
+#include <event2/event.h>
+#include <event2/http.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
+#include <event2/keyvalq_struct.h>
+
+int main(int argc, char* argv[]){
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
