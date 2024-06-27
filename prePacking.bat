22b63f1fdbe92880e3c6e7816ac5b7feb7a00608
WuChang <3142324836@qq.com>
2024-06-27 22:31:13 +0800

    Add CI

---
+++ prePacking.bat
@@ -0,0 +1,9 @@
+﻿del /f /s /q %1\logs\*
+rmdir /s /q %1\logs
+
+del /f /s /q %1\*.iobj
+del /f /s /q %1\*.ipdb
+del /f /s /q %1\*.exp
+del /f /s /q %1\*.ilk
+del /f /s /q %1\*.lib
+del /f /s /q %1\*.a
