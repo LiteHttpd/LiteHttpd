d351d671637a21d9d48949af4cab3df1f19f8787
WuChang <3142324836@qq.com>
2024-06-28 22:30:53 +0800

    Add prePacking.ps1

---
+++ prePacking.ps1
@@ -0,0 +1,11 @@
+﻿$LOG_PATH = "$($args[0])/logs"
+if ((Test-Path $LOG_PATH) -eq "True") {
+	Remove-Item $LOG_PATH -Recurse
+}
+
+Remove-Item "$($args[0])/*" -Include "*.iobj"
+Remove-Item "$($args[0])/*" -Include "*.ipdb"
+Remove-Item "$($args[0])/*" -Include "*.exp"
+Remove-Item "$($args[0])/*" -Include "*.ilk"
+Remove-Item "$($args[0])/*" -Include "*.lib"
+Remove-Item "$($args[0])/*" -Include "*.a"
