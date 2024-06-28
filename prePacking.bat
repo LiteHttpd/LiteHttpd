82b1568859f03cc25eae80d09676b59822e880df
WuChang <3142324836@qq.com>
2024-06-28 23:29:59 +0800

    Removed prePacking.bat

--- prePacking.bat
+++
@@ -1,9 +0,0 @@
-﻿del /f /s /q %1\logs\*
-rmdir /s /q %1\logs
-
-del /f /s /q %1\*.iobj
-del /f /s /q %1\*.ipdb
-del /f /s /q %1\*.exp
-del /f /s /q %1\*.ilk
-del /f /s /q %1\*.lib
-del /f /s /q %1\*.a
